500633ead8702e40e21bf6472d79fec7c13bd7b1
John Crist <john.crist@outlook.com>
2022-02-06 15:04:32 -0700

    Added Aggressive Mode, changed switches, menu text

--- RyzenSelect.ps1
+++ RyzenSelect.ps1
@@ -13,26 +13,37 @@ function acOn {
     powercfg -setacvalueindex scheme_current sub_processor PERFBOOSTMODE 1
     powercfg -setdcvalueindex scheme_current sub_processor PERFBOOSTMODE 0
 }
+
+function aggOn {
+    powercfg -setacvalueindex scheme_current sub_processor PERFBOOSTMODE 2
+    powercfg -setdcvalueindex scheme_current sub_processor PERFBOOSTMODE 2
+}
 function promptMode {
     $powerMode = Read-Host "Which power mode would you like (1, 2)"
     switch ($powerMode) {
         1 {
-            Write-Host "`nSetting Ryzen Boost to ON...`n"
+            Write-Host "`nSetting Ryzen Boost to ENABLED...`n"
             Start-Sleep -s 3
             boostOn
             $null = Read-Host 'Ryzen Boost is set to ON. Press any key to exit'
         }
         2 {
-            Write-Host "`nSetting Ryzen Boost to OFF...`n"
-            Start-Sleep -s 3
-            boostOff
-            $null = Read-Host 'Ryzen Boost is set to OFF. Press any key to exit'
-        }
-        3 {
-            Write-Host "`nSetting Ryzen Boost to ON only when plugged in...`n"
+            Write-Host "`nSetting Ryzen Boost to ENABLED on AC POWER only...`n"
             Start-Sleep -s 3
             acOn
-            $null = Read-Host 'Ryzen Boost is set to ON only when plugged in. Press any key to exit'
+            $null = Read-Host 'Ryzen Boost is set to ENABLED on AC POWER only. Press any key to exit'
+        }
+        3 {
+            Write-Host "`nSetting Ryzen Boost to AGGRESSIVE...`n"
+            Start-Sleep -s 3
+            aggOn
+            $null = Read-Host 'Ryzen Boost is set to AGGRESSIVE. Press any key to exit'
+        }
+        4 {
+            Write-Host "`nSetting Ryzen Boost to DISABLED...`n"
+            Start-Sleep -s 3
+            boostOff
+            $null = Read-Host 'Ryzen Boost is set to DISABLED. Press any key to exit'
         }
         default {
             Write-Host "`n`nYou didn't select a valid menu option.`n"
@@ -46,22 +57,26 @@ function writeMenu {
     Write-Host "Ryzen Boost Mode Selection Utility"
     Write-Host "Written by John Crist (john.crist@outlook.com)`n"
     Write-Host "Please select from the following performance modes:"
-    Write-Host "1) Ryzen Boost ON (higher CPU performance)"
-    Write-Host "2) Ryzen Boost OFF (power efficiency, lower temperatures)"
-    Write-Host "3) Ryzen Boost ON when plugged in, OFF when on battery`n"
+    Write-Host "1) Ryzen Boost ENABLED (higher CPU performance)"
+    Write-Host "2) Ryzen Boost ENABLED on AC POWER only (power efficiency, lower temperatures)"
+    Write-Host "3) Ryzen Boost AGGRESSIVE (highest CPU performance)"
+    Write-Host "4) Ryzen Boost DISABLED`n"
     promptMode
 }
 
 $modeSwitch = $args[0]
 switch ($modeSwitch) {
-    "on" {
+    "-enabled" {
         boostOn
     }
-    "off" {
-        boostOff
-    }
-    "ac" {
+    "-enabledonac" {
         acOn
+    }
+    "-aggressive" {
+        aggOn
+    }
+    "-disabled" {
+        boostOff
     }
     default {
         writeMenu
